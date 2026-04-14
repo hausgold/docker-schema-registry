79ee9671ba048721879a52ef3f6c300543ceeb9f
Hermann Mayer <hermann.mayer92@gmail.com>
2026-04-14 09:11:57 +0200

    Added version 8.0, 8.1, 8.2 and upgraded the patch levels of 7.8 and 7.9.
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

---
+++ 8.0/build/config/schema-registry.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Configure application defaults
+export SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS=${SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS:-'kafka.local:9092'}
+export SCHEMA_REGISTRY_HOST_NAME=${SCHEMA_REGISTRY_HOST_NAME:-${MDNS_HOSTNAME}}
+export SCHEMA_REGISTRY_LISTENERS=${SCHEMA_REGISTRY_LISTENERS:-'http://0.0.0.0:8081'}
+
+# Start the original bootstrapping
+exec /etc/confluent/docker/run
